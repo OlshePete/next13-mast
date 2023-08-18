b8f23e41bb9c0862e48bea85fd88fb86998a9e35
OlshePete <olshevskii.pv@gmail.com>
2023-08-18 10:53:48 +0300

    fix devtools warning

--- app/components/HeaderContent/HeaderLogo.jsx
+++ app/components/HeaderContent/HeaderLogo.jsx
@@ -6,7 +6,7 @@ import { HeaderLogoIcon } from "../CustomIcons";
 const HeaderLogo = () => {
   return (
     <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
-      <HeaderLogoIcon show={true} isDark={false} />
+      <HeaderLogoIcon show={true} isdark={false} />
     </Flex>
   )
 }
